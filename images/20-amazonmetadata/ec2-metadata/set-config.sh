331dd172d61b98c10c6a4febb8848058a701d55e
benok <benok1@gmail.com>
2022-01-24 18:26:53 +0900

    delete non console images

--- images/20-amazonmetadata/ec2-metadata/set-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[ -f "$1" ] || exit 1
-
-ros config merge < "${1}"
--- images/20-zfs/entry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mount --rbind /host/dev /dev > /dev/null 2>&1
-zpool import -a > /dev/null 2>&1
-exec "$@"
